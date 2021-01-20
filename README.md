7073df6fd1a3f4b5a5fa80b7cf1568c4dcfa6659
290119ali <donitasali10@gmail.com>
2021-01-19 19:30:30 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zx76ld
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zx76ld)
---
+++ src/app/products.ts
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: 1,
+    name: "Ali adonis",
+    price: 799,
+    description: "Balatazar"
+  },
+  {
+    id: 2,
+    name: "Adonis ali",
+    price: 699,
+    description: "Blancas"
+  },
+  {
+    id: 3,
+    name: "Mas ali",
+    price: 299,
+    description: ""
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
